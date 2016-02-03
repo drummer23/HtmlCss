20c2ce834e8a11606a4837b399d7e1e385d5343d
Roger Spielmann <rspielmann@snowflake.ch>
2016-02-03 17:22:50 +0100

    devtools

--- examples/devtools/custom.js
+++ examples/devtools/custom.js
@@ -1,13 +1,3 @@
-var obj = {
-    do7 : function x() {
-        window.alert(7);
-    },
-
-
-     do11 : function someFunction() {
+function someFunction() {
         window.alert(5 + 6);
-    }
-};
-
-
-obj.do7();
+}
